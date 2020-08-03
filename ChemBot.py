d08c8a2c4e733561e3b8cac7500d96eff6271641
Ruiran Xun <xun.ruiran@gmail.com>
2020-08-03 19:16:18 -0400

    updated to headless Chrome

--- ChemBot.py
+++ ChemBot.py
@@ -21,7 +21,13 @@ cs = ChemSpider(CHEMSPIDER_TOKEN)
 
 client = commands.Bot(command_prefix='!')
 
-driver = webdriver.Firefox()
+op = webdriver.ChromeOptions()
+op.binary_location = os.getenv('GOOGLE_CHROME_BIN')
+op.add_argument('--headless')
+op.add_argument('--no-sandbox')
+op.add_argument('--disable-dev-sh-usage')
+
+driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH'), chrome_options=op)
 driver.get('https://www.sigmaaldrich.com/united-states.html')
 
 @client.event
